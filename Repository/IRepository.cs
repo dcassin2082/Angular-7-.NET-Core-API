71eaed5e7a75de27ce24d0c59b7130b7690468bc
dcassin2082 <dcassin2082@gmail.com>
2019-11-03 04:43:07 -0700

    Add files via upload

---
+++ Repository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace WebApi.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>> predicate);
+        Task<T> Get(int id);
+        Task<T> Get(Expression<Func<T, bool>> predicate);
+        Task<T> Post(T entity);
+        Task<T> Put(T entity);
+        Task<T> Delete(T entity);
+
+    }
+}
